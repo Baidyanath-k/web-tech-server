e4eb07f22fbab778a0487e9b34bc6396bcbace01
Baidyanath-K <baidyanathkumar12091998@gmail.com>
2022-10-27 08:18:19 +0600

    server link setup

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const details = require('./data/details.json');
 app.get('/',(req,res)=>{
     res.send('Running')
 });
+
 app.get('/learning-catagories', (req, res) => {
     res.send(catagory);
 })
@@ -18,4 +19,26 @@ app.get('/learning-catagories/:id', (req, res) => {
     const id=req.params.id;
     const selectedcatagory=catagory.find(n=>n.id===id);
     res.send(selectedcatagory);
+})
+
+app.get('/details', (req, res) => {
+    res.send(details);
+})
+
+app.get('/details/:id', (req, res) => {
+    const id=req.params.id;
+    const selectedDetails=details.find(n=>n._id===id);
+    res.send(selectedDetails);
+})
+
+app.get('/details-category/:id', (req,res)=>{
+    const id=req.params.id;
+   const categoryDetails=details.filter(n=>n.category_id===id);
+    res.send(categoryDetails); 
+    
+})
+
+
+app.listen(port,()=>{
+    console.log('Running3',port)
 })
