a84824b58a84bf285aa9feb1cf2c173c4a67511a
Baidyanath-K <baidyanathkumar12091998@gmail.com>
2022-10-27 08:14:57 +0600

    check serversite

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const express =require('express');
 const app=express();
 const cors = require('cors');
 const port=process.env.PORT || 5000
+
+app.use(cors());
+
+const catagory=require('./Data/category.json');
+const details = require('./data/details.json');
+
+app.get('/',(req,res)=>{
+    res.send('Running')
+});
